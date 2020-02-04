73685d6a34d762bc98e2c427255c33418b03870b
db5n17 <db5n17@soton.ac.uk>
2020-02-04 04:43:34 +0000

    Completed tasks 1 to 3.

--- sheet_1.hs
+++ sheet_1.hs
@@ -1,7 +1,20 @@
 -- Task 1
-zipL :: [Int] -> [Int] -> [[Int]]
-zipL [] _ = []
-zipL (x:xs) (y:ys) | (length xs == length ys) = [x, y] : zipL xs ys
-                   | otherwise                = []
+zipLE :: [a] -> [a] -> [[a]]
+zipLE [] _ = []
+zipLE (x:xs) (y:ys) | (length xs == length ys) = [x, y] : zipL xs ys
+                    | otherwise                = []
+
+unzipLE :: [[a]] -> ([a], [a])
+unzipLE xss = ([ xs !! 0 | xs <- xss ], [ xs !! 1 | xs <- xss ]) 
 
 -- Task 2
+zipL :: [a] -> [a] -> [[a]]
+zipL [] [] = []
+zipL [] (y:ys) = [y] : zipL [] ys
+zipL (x:xs) [] = [x] : zipL xs []
+zipL (x:xs) (y:ys) = [x, y] : zipL xs ys
+
+-- Task 3
+multiZipL :: [[a]] -> [[a]]
+multiZipL [] = []
+multiZipL xss = [ head xs | xs <- xss ] : multiZipL (filter (not . null) (map (tail) xss))
